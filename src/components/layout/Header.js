bf7e39efd5a0432abca69477ba9f27cad82934c3
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2019-04-16 02:29:15 -0400

    First Code Upload

---
+++ src/components/layout/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+    return (
+        <header style={headerStyle}>
+            <h1>TodoList</h1>
+            <Link style={linkStyle} to="/react-todo/">Home</Link>
+            <span> | </span>
+            <Link style={linkStyle} to="/react-todo/about">About</Link>
+        </header>
+    )
+}
+const linkStyle = {
+    color: '#fff',
+    textDecoration: 'none'
+}
+const headerStyle = {
+    background: '#333',
+    color: '#fff',
+    textAlign: 'center',
+    padding: '10px'
+}
