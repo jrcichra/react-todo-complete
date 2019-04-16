33e675e301f3a3956a1f7ec1fdb47e5ccfcc0473
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2019-04-16 02:40:30 -0400

    Try again

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5fb0d6ffa3bd06861eb92fa616fc2186.js"
+  "/react-todo-complete/precache-manifest.6e9bc1158c254ecde2980108fc8f2935.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-todo-complete/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
