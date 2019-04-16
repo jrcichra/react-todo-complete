6e4892eeac81bb76c3b49b5b39fb33738e10a4d7
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2019-04-16 02:42:27 -0400

    removed slashes

--- src/App.js
+++ src/App.js
@@ -54,13 +54,13 @@ class App extends Component {
                 <div className="App">
                     <div className="container">
                         <Header />
-                        <Route exact path="/react-todo-complete/" render={props => (
+                        <Route exact path="react-todo-complete/" render={props => (
                             <React.Fragment>
                                 <AddTodo addTodo={this.addTodo} />
                                 <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
                             </React.Fragment>
                         )} />
-                        <Route path="/react-todo-complete/about" component={About} />
+                        <Route path="react-todo-complete/about" component={About} />
                     </div>
                 </div>
             </Router>
